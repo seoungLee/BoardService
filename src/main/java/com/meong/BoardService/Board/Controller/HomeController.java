1e49a4d62546228808c22382d908b72cc5880fe0
seounglee94 <dlrpwkd@naver.com>
2023-05-24 14:11:00 +0900

    메인페이지에 list 띄우기 수정완료

--- src/main/java/com/meong/BoardService/Board/Controller/HomeController.java
+++ src/main/java/com/meong/BoardService/Board/Controller/HomeController.java
@@ -6,34 +6,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/board")
-public class BoardController {
+public class HomeController {
     private final BoardService boardService;
 
-    /*@GetMapping("/")
+    @GetMapping("/")
     public String findAll(Model model){
         List<BoardDTO> boardDTOList = boardService.findAll();
         model.addAttribute("boardList", boardDTOList);
-        return "index";
-    }*/
-
-    @GetMapping("/write")
-    public String writeForm(){
-        return "/board/write";
-    }
-
-    @PostMapping("/write")
-    public String write(@ModelAttribute BoardDTO boardDTO){
-        System.out.println("boardDTO = " + boardDTO);
-        boardService.save(boardDTO);
         return "index";
     }
 }
